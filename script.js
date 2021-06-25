61d38e93b17afe5be7dd2e73f62622ca16f242dc
mtg-tech <71662131+mtg-tech@users.noreply.github.com>
2021-06-25 23:58:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function sync(){
+    let selectInputValue = document.getElementById("searchBox").value;
+    console.log(selectInputValue);
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://192.168.43.65/cgi-bin/doc_cmd.py?var="+selectInputValue, true);
+    xhr.send();
+    xhr.onload = function (){
+      let output = xhr.responseText;
+      document.getElementById("container").innerHTML = output;
+    }
+  }   
+  
+
